e4cec2abfa7aad7ee713df75af6f5d3a1417c1d3
Christian Puchinger <puchinger@burda.com>
2021-11-03 12:30:37 +0100

    Update HBMMessengerExtension.php

--- DependencyInjection/HBMMessengerExtension.php
+++ DependencyInjection/HBMMessengerExtension.php
@@ -23,7 +23,7 @@ class HBMMessengerExtension extends Extension {
 
     $container->setParameter('hbm.messenger', $config);
 
-    $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
-    $loader->load('services.yml');
+//    $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+//    $loader->load('services.yml');
   }
 }
